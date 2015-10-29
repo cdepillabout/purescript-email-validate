084c5b556ec207451c6e6a4bf0a0c741c1d4ec72
(cdep)illabout <cdep.illabout@gmail.com>
2015-10-29 16:12:52 +0900

    Update tests.

--- test/Main.purs
+++ test/Main.purs
@@ -10,9 +10,11 @@ import Test.Spec.Runner (Process(), run)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 
+import Text.Email.Parser
+
 main :: forall e . Eff (console :: CONSOLE, process :: Process | e) Unit
 main = run [consoleReporter] do
     describe "email validation" do
         it "passes unit tests" do
-                let isAwesome = true
-                isAwesome `shouldEqual` true
+            let isAwesome = true
+            isAwesome `shouldEqual` true
